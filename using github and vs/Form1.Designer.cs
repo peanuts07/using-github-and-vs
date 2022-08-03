fad58dbfa6b2059ea16dd94b01614cb2d8a35062
peanuts07 <keirapienaar@icloud.com>
2022-08-03 11:58:59 +0200

    Add project files.

---
+++ using github and vs/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace using_github_and_vs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserTemp = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUserTemp
+            // 
+            this.txtUserTemp.Location = new System.Drawing.Point(355, 65);
+            this.txtUserTemp.Name = "txtUserTemp";
+            this.txtUserTemp.Size = new System.Drawing.Size(125, 27);
+            this.txtUserTemp.TabIndex = 1;
+            this.txtUserTemp.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(355, 117);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(94, 29);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PLEASE ENTER YOUR TEMPERATURE:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 168);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtUserTemp);
+            this.Name = "Form1";
+            this.Text = "FEVER APP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtUserTemp;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ using github and vs/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace using_github_and_vs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool btnClick;
+        int userTemp;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            userTemp = Convert.ToInt32(txtUserTemp.Text);
+            btnClick = true;
+
+            if (userTemp == 38 || userTemp > 38)
+                {
+                    MessageBox.Show("FEVER ALERT", "ALERT", MessageBoxButtons.OK);
+
+                }
+            else if (txtUserTemp.Text.Length == 0) 
+                {
+                    MessageBox.Show("PLEASE ENTER A TEMPERATURE", "ERROR", MessageBoxButtons.OK);
+                }
+
+            else
+                {
+                     MessageBox.Show("SLAY", "SLAY", MessageBoxButtons.OK);
+                }
+        }
+    }
+}
